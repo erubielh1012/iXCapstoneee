46e2a86382dd4494f95b6cb0a7011ca7a1968a15
erubielh1012 <erhu2729@colorado.edu>
2024-07-13 14:42:17 -0600

    WIP

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,49 +1,43 @@
 const p = require('path');
-require("dotenv").config({ path: p.resolve(__dirname, '../.env') });
-
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config({ path: p.resolve(__dirname, '../.env') });
+
+const blogRoutes = require("./routes/blogRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const authRoutes = require("./routes/authRoutes");
+
+const path = require("path")
+const app = express();
 
 const port = process.env.PORT || 8000;
 
 console.log("MongoDB URI from environment:", process.env.MONGO_URI);
 
-// const connectDB = require("./database/db");
-// connectDB();
 // connecting to MongoDB Atlas
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const uri = process.env.MONGO_URI 
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
   try {
     await client.connect();
-    await client.db("blog").command({ ping: 1 });
-    console.log("Pinged your deployment. Successfully connected to MongoDB!");
+    await client.db("onlineData").command({ ping: 1 });
+    console.log("Pinged your deployment. You're connected to", client)
+  } catch (err) {
+    console.log('Error connection to MongoDB', err);
   } finally {
     await client.close();
   }
 }
 run().catch(console.dir);
 
-const blogRoutes = require("./routes/blogRoutes");
-const categoryRoutes = require("./routes/categoryRoutes");
-const authRoutes = require("./routes/authRoutes");
-const path = require("path")
-
-const app = express();
-
+const connectDB = require("./database/db");
+connectDB();
 // Cross-origin resource sharing is a mechanism that allows a web page to access restricted resources from a server on a domain different than the domain that served the web page.
 // Enabling CORS for any unknown origin
 app.use(cors());
-
 app.use(express.json());
 
 app.use("/api/blogs", blogRoutes);
