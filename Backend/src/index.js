3e27448f28d164bc5910c53b774f97d9a8c7023f
erubielh1012 <erhu2729@colorado.edu>
2024-07-30 15:37:53 -0600

    fix: refactor hard coded connection string to Heroku config var

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,9 +2,10 @@ const p = require('path');
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config({ path: p.resolve(__dirname, '../.env') });
+const uri = process.env.MONGODB_URI;
 
-const blogRoutes = require("./routes/blogRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
+const blogRoutes = require("./routes/blogRoutes");
 const authRoutes = require("./routes/authRoutes");
 
 const path = require("path")
